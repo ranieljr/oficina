4e3f32a2b98089f4421d2f08edb199a76576034b
RANIEL ANTONIO CORTE JUNIOR <raniel_junior@hotmail.com>
2025-05-05 10:46:33 -0400

    excel

--- backend/src/main.py
+++ backend/src/main.py
@@ -78,6 +78,5 @@ def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run()
-
-app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
